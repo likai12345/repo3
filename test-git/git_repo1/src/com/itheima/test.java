e386fe8303ca82675f8fa3f508b6e7e24a8bdb02
zhaoge <zhaoge@163.com>
2018-12-26 17:09:16 +0800

    第三次修改

--- test-git/git_repo1/src/com/itheima/test.java
+++ test-git/git_repo1/src/com/itheima/test.java
@@ -3,5 +3,9 @@ package com.itheima;
 public class test {
     public static void main(String[] args) {
         System.out.println("hello");
+        int i = 1;
+        int j = 2;
+
+        System.out.println("world");
     }
 }
